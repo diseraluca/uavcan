46053cfc373f9952d6b72abf78f7367c1f92ca7f
Luca Di Sera <disera.luca@gmail.com>
2021-06-01 13:26:19 +0200

    Moves unsafe-op-in-unsafe-code from allow to deny.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![no_std]
 #![feature(cfg_eval)]
-#![deny(noop_method_call, single_use_lifetimes, unreachable_pub, unsafe_code)]
+#![deny(noop_method_call, single_use_lifetimes, unreachable_pub, unsafe_code, unsafe_op_in_unsafe_fn)]
 
 pub mod rx;
 pub mod session_id;
